8be72f251da93aeabc379daa9b9197bf852baaf7
Julien Amblard <amblard.julien@gmail.com>
2019-12-02 13:10:28 +0100

    start rework

--- demo/dev.js
+++ demo/dev.js
@@ -1,36 +1,7 @@
-// import Pixels from "../src/"
+import Pixels from "../src/"
 import "../src/core.scss"
-import Direction from "../src/direction" 
-const imgs = [...document.querySelectorAll(".item img")]
-imgs.map( o => {
-	new Direction({
-		el: o
-	})
-	// new Pixels(o, {
-	// 	onUpdate: () => console.log( "update" ),
-	// 	size: 15
-	// })
+new Pixels({
+	$el: document.querySelectorAll(".item"),
+	onUpdate: () => console.log( "update" ),
+	size: 15
 })
-// trolol.start()
-// import Pixel from "../src/"
-
-// var aThumbs = document.querySelectorAll('.item');
-// var aPixel = [];
-
-
-// var init = function(){
-
-// 	for (var i = 0; i < aThumbs.length; i++) {
-
-// 		aPixel.push( new Pixel( aThumbs[i].querySelector('img'), {
-
-// 			w : 10,
-// 			preload : true
-			
-// 		}) );
-
-// 	};
-
-// }
-
-// window.onload = init;
